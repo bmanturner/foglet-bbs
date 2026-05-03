9da8644c151d453ea6c2adfd1bab976fd0b8eaa7
Content Designer <content-designer@paperclip.local>
2026-05-03 11:50:02 -0500

    FOG-467 isolate render cadence dispatcher test

--- test/foglet_bbs/tui/render_cadence_test.exs
+++ test/foglet_bbs/tui/render_cadence_test.exs
@@ -19,6 +19,15 @@ defmodule Foglet.TUI.RenderCadenceTest do
   end
 
   setup do
+    Process.flag(:trap_exit, true)
+
+    on_exit(fn ->
+      case Process.whereis(Dispatcher) do
+        nil -> :ok
+        pid -> _ = catch_exit(GenServer.stop(pid, :normal, 1_000))
+      end
+    end)
+
     initial_state = %{
       app_module: TestApp,
       model: %{count: 0},
@@ -29,13 +38,7 @@ defmodule Foglet.TUI.RenderCadenceTest do
       command_registry_table: :render_cadence_command_registry
     }
 
-    dispatcher =
-      start_supervised!(%{
-        id: {Dispatcher, self()},
-        start: {Dispatcher, :start_link, [self(), initial_state, [name: nil]]},
-        restart: :temporary
-      })
-
+    {:ok, dispatcher} = Dispatcher.start_link(self(), initial_state)
     %{dispatcher: dispatcher}
   end
 
